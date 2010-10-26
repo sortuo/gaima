e9aef584b35691d2b72e9499d82085e2c884ff72
sortuo@gmail.com <sortuo@gmail.com@cf77f272-00b9-11de-b507-8b4ecd5210be>
2010-10-26 07:54:58 +0000

    added enum

--- gaima/src/fractal/Fractal.java
+++ gaima/src/fractal/Fractal.java
@@ -1,12 +1,24 @@
 package fractal;
 
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Collection;
 
+/**
+ * @author ts
+ * 
+ * Fractals are created with this Fractal class and different rules are stated with Enum Rules or by calling methods. 
+ * You can combine different rules with the proram parameters.
+ * 0xFF000000 is the base blue color for images.
+ *
+ */
 public class Fractal {
 
+	/**
+	 * @author sortuo Rules for fractal generation.
+	 */
 	public enum Rules {
-		ABS, SQRT, TAN, COS, TEST1, TEST2, TEST3, DRIPPINGMATRIX, MANDELBROT
+		ABS, SQRT, TAN, COS, TEST1, TEST2, TEST3, TEST4, DRIPPINGMATRIX, MANDELBROT, MANDELBROT_MOD1, MANDELBROT_BLACKLAGOON
 	}
 
 	public static int countStep(int x, int y) {
@@ -36,43 +48,46 @@ public class Fractal {
 	public static int countVariable(int x, int y, int offset,
 			Collection<Rules> variables) {
 		int result = 0;
-		for (Rules rank : variables) {
-			result += countWithEnum(x, y, rank);
+		for (Rules rule : variables) {
+			result += countWithEnum(x, y, rule, offset);
 		}
 		return offset + result;
 	}
 
-	public static int countWithEnum(int x, int y, Rules r) {
-		int result = 0;
+	public static int countWithEnum(int x, int y, Rules r, int offset) {
+		int result = offset;
 		switch (r) {
 		case SQRT:
-			result = (int) (Math.sqrt(x) + Math.sqrt(y));
+			result += (int) (Math.sqrt(x) + Math.sqrt(y));
 			break;
 		case ABS:
-			result = Math.abs(x) + Math.abs(y);
+			result += Math.abs(x) + Math.abs(y);
 			break;
 		case TAN:
-			result = (int) (Math.tan(x) + Math.tan(y));
+			result += (int) (Math.tan(x) + Math.tan(y));
 			break;
 		case COS:
-			result = (int) (Math.cos(x) + Math.cos(y));
+			result += (int) (Math.cos(x) + Math.cos(y));
 			break;
 		case TEST1:
-			result = (int) Math.max(x, y) - Math.min(y, x);
+			result += (int) Math.max(x, y) - Math.min(y, x);
 			break;
 		case DRIPPINGMATRIX:
-			result = (int) (Math.abs((Math.tan(x) * 1.02 * Math.sqrt(x) * 1.02)
-					* Math.sqrt(y)));
+			result += (int) (Math
+					.abs((Math.tan(x) * 1.02 * Math.sqrt(x) * 1.02)
+							* Math.sqrt(y)));
 			break;
 		case TEST3:
-			result = 4 * (x * y / (x + y));
+			result += 4 * (x * y / (x + y));
 			break;
 
 		case MANDELBROT:
 			// double x0 = -2.5 + x*0.005; //3.5
 			// double y0 = -1+ y*0.006; //2
-			double x0 = -2 + (x / (BuildFractal.width * 0.37)); // 3.5 total range
-			double y0 = -1.2 + (y / (BuildFractal.height * 0.4)); // 2 total range 
+			double x0 = -2 + (x / (BuildFractal.width * 0.37)); // 3.5 total
+			// range
+			double y0 = -1.2 + (y / (BuildFractal.height * 0.4)); // 2 total
+			// range
 
 			double xx = 0;
 			double yy = 0;
@@ -92,8 +107,93 @@ public class Fractal {
 			if (iteration == max_iteration) {
 				result = 0;
 			} else {
-				result = iteration;
+				result += iteration;
 			}
+
+			break;
+
+		case MANDELBROT_MOD1:
+			int blue = Color.blue.getRGB();
+			int green = Color.green.getRGB();
+			int red = Color.red.getRGB();
+
+			result = green / 2;
+			x0 = -2 + (x / (BuildFractal.width * 0.37)); // 3.5 total
+			// range
+			y0 = -1.2 + (y / (BuildFractal.height * 0.4)); // 2 total
+			// range
+
+			xx = 0;
+			yy = 0;
+
+			iteration = 0;
+			max_iteration = 1000;
+
+			while (xx * xx + yy * yy <= (2 * 2) && iteration < max_iteration) {
+				double xtemp = Math.sqrt(xx * yy) - Math.sqrt(yy * xx) + x0;
+				yy = 2 * xx / 2 * yy + y0;
+
+				xx = xtemp;
+
+				iteration = iteration + 1;
+			}
+
+			if (iteration == max_iteration) {
+				result = 0;
+			} else {
+				result += iteration;
+			}
+
+			break;
+
+		case MANDELBROT_BLACKLAGOON:
+			result = 0xFF000000;
+			x0 = -2 + (x / (BuildFractal.width * 0.37)); // 3.5 total
+			// range
+			y0 = -1.2 + (y / (BuildFractal.height * 0.4)); // 2 total
+			// range
+
+			xx = 0;
+			yy = 0;
+
+			iteration = 0;
+			max_iteration = 1000;
+
+			while (xx * xx + yy * yy <= (2 * 2) && iteration < max_iteration) {
+				double xtemp = xx * xx - yy * yy + x0;
+				yy = 2 * xx / 2 * yy + y0;
+
+				xx = xtemp;
+
+				iteration = iteration + 1;
+			}
+
+			if (iteration == max_iteration) {
+				result = 0;
+			} else {
+				result += iteration;
+			}
+
+			break;
+
+		case TEST4:
+			blue = Color.blue.getRGB();
+			green = Color.green.getRGB();
+			red = Color.red.getRGB();
+
+			result = blue;
+			x0 = -2 + (x / (BuildFractal.width * 0.37)); // 3.5 total
+			// range
+			y0 = -1.2 + (y / (BuildFractal.height * 0.4)); // 2 total
+			// range
+
+			xx = 0;
+			yy = 0;
+
+			iteration = 0;
+			max_iteration = 1000;
+
+			result += (int) Math.sqrt(x * y) * (y);
 
 			break;
 
@@ -102,6 +202,8 @@ public class Fractal {
 		}
 		return result;
 	}
+
+	// OLD TESTS before refactoring.
 	// int red = (y * 255) / (h - 1);
 	// int blue = (x * 255) / (w - 1);
 	// pix[index++] = (255 << 24) | (red << 16) | blue;
